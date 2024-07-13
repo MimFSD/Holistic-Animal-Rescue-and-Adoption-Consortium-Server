01df970446e709bcd9701ef7dbb44ee8dff5cc15
MimFSD <mimislam878@gmail.com>
2024-07-13 15:28:44 +0600

    Verify token and verify admin applied

--- index.js
+++ index.js
@@ -378,10 +378,10 @@ async function run() {
             res.send(result)
         })
 
-               // await client.db("admin").command({ ping: 1 });
+            // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-       // Ensures that the client will close when you finish/error
+         // Ensures that the client will close when you finish/error
                   // await client.close();
     }
 }
