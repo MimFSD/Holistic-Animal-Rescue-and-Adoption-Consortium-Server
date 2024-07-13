9a58b3c799324a5a95c300f46962a18a182ce383
MimFSD <mimislam878@gmail.com>
2024-07-13 15:24:07 +0600

    Added e nv file again in project

--- index.js
+++ index.js
@@ -370,7 +370,7 @@ async function run() {
             res.send(result)
         })
 
-        // Delete a payment data from db
+                  // Delete a payment data from db
         app.delete('/delete-donation/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
